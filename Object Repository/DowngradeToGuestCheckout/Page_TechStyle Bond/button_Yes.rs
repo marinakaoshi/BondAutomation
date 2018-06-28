19df77c6eb906cad75b028495e7186d89d80521a
MARI <MARI@10.50.202.98>
2018-06-28 11:55:10 -0700

    UPDATE: Added simple error handling for DowngradeToGuestCheckout Test Case
    Signed-off-by: MARI <MARI@10.50.202.98>

---
+++ Object Repository/DowngradeToGuestCheckout/Page_TechStyle Bond/button_Yes.rs
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>button_Yes</name>
+   <tag></tag>
+   <elementGuidId>083be591-a450-4090-922a-6f0027967c0a</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>button</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>type</name>
+      <type>Main</type>
+      <value>button</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>btn btn-primary</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>ng-click</name>
+      <type>Main</type>
+      <value>confirm(true)</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Yes</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;ngdialog2&quot;)/div[@class=&quot;ngdialog-content&quot;]/div[@class=&quot;panel panel-dull&quot;]/div[@class=&quot;panel-footer clearfix&quot;]/div[@class=&quot;col-md-12 text-center&quot;]/button[@class=&quot;btn btn-primary&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
--- Scripts/Downgrade to GuestCheckout/Script1530209219233.groovy
+++ Scripts/Downgrade to GuestCheckout/Script1530209219233.groovy
@@ -18,7 +18,26 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKeywords
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
 
 WebUI.openBrowser('')
+
 WebUI.navigateToUrl('https://bond.techstyle.net')
+
 CustomKeywords.'bondAutomation.bondAutomation.identityLoginKey'('')
+
+CustomKeywords.'bondAutomation.bondAutomation.searchCustomerID'('')
+
+WebUI.waitForAngularLoad(5)
+
+if (WebUI.verifyElementPresent(findTestObject('DowngradeToGuestCheckout/Page_TechStyle Bond/button_Guest Checkout'),10, FailureHandling.OPTIONAL)) {
+	
+	WebUI.click(findTestObject('DowngradeToGuestCheckout/Page_TechStyle Bond/button_Guest Checkout'))
+	WebUI.click(findTestObject('DowngradeToGuestCheckout/Page_TechStyle Bond/button_Make Guest'))
+	WebUI.click(findTestObject('DowngradeToGuestCheckout/Page_TechStyle Bond/button_Yes'))
+}
+else{
+	System.out.print("no guestcheckout button")
+}
+
+
